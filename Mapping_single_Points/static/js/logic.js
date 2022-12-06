ad23a8192ac1de10144f474070ffdd7b9851f22a
shawnkitagawa <seankitagawa@gmail.com>
2022-12-06 13:24:23 -0800

    update

--- Mapping_single_Points/static/js/logic.js
+++ Mapping_single_Points/static/js/logic.js
@@ -2,7 +2,23 @@
 console.log("working");
 // Create the map object with a acenter and zoom level.
 
-let map = L.map("mapid").setView([34.0522,-118.2437], 4);
+
+let map = L.map("mapid").setView([36.1733, -120.1794], 5);
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [37.615223, -122.389977],
+    [30.1974292,-97.6663058],
+    [43.67771760000001, -79.62481969999999],
+    [40.641766,-73.780968]
+  ];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color:"blue",
+    opacity: 0.5,
+    weight: 4,
+    dashArray: "10 10"
+}).addTo(map);
+
 //  Add a marker to the map for Los Angeles, California.
 /*L.circle([34.0522, -118.2437],
     {
@@ -18,6 +34,7 @@ let map = L.map("mapid").setView([34.0522,-118.2437], 4);
         }).addTo(map);
         */
   // Loop through the cities array and create one marker for each city.
+  /*
   let cityData = cities;
   cityData.forEach(function(city)
   {
@@ -33,6 +50,7 @@ let map = L.map("mapid").setView([34.0522,-118.2437], 4);
   
   });
 
+  */
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
